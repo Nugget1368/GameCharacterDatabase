9f78b2be869e543b497e525a5592135eace35842
Nugget1368 <rneg1@hotmail.com>
2024-11-13 18:38:04 +0100

    Created:
    - FactionModel
    - Many-to-Many relationship
    - Faction Migration

--- GameCharacterDatabase/Data/DataContext.cs
+++ GameCharacterDatabase/Data/DataContext.cs
@@ -12,5 +12,6 @@ namespace GameCharacterDatabase.Data
 		public DbSet<Character> Characters { get; set; }
 		public DbSet<Backpack> Backpacks { get; set; }
 		public DbSet<Weapon> Weapons { get; set; }
+		public DbSet<Faction> Factions { get; set; }
 	}
 }
---
+++ GameCharacterDatabase/Models/Faction.cs
@@ -0,0 +1,10 @@
+﻿namespace GameCharacterDatabase.Models
+{
+	public class Faction
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public List<Character> Characters { get; set; }
+
+	}
+}
